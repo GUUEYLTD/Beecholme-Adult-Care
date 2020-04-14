86b739386f3450848c23261dbd572dc79dc1758f
d-horbachov-dev <dmytriy.horbachov@gmail.com>
2020-04-14 15:44:29 +0300

    booking page

--- wp-content/themes/BAC/booking.php
+++ wp-content/themes/BAC/booking.php
@@ -1,5 +1,6 @@
 <?php /*Template Name: Booking */
  get_header();
+ /** @var WP_User $user */
 ?>
 <div class="booking-page mid-container d-flex">
   <div class="booking-form">
@@ -7,7 +8,11 @@
     <?php echo do_shortcode('[ameliabooking employee='.$userId.']'); ?>
   </div>
   <div class="about-info">
+    <?php
 
+      get_template_part('template-parts/counsellors/booking-counsellor-information');
+
+    ?>
   </div>
 </div>
 <?php get_footer(); ?>
