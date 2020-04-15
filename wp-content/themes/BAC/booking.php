730edf45dc6ac6fe4efc97eb36717610b651106e
d-horbachov-dev <dmytriy.horbachov@gmail.com>
2020-04-15 20:12:55 +0300

    booking page fixes

--- wp-content/themes/BAC/booking.php
+++ wp-content/themes/BAC/booking.php
@@ -1,18 +1,14 @@
 <?php /*Template Name: Booking */
  get_header();
- /** @var WP_User $user */
 ?>
+
 <div class="booking-page mid-container d-flex">
   <div class="booking-form">
     <?php $userId = $_GET['user']; ?>
     <?php echo do_shortcode('[ameliabooking employee='.$userId.']'); ?>
   </div>
   <div class="about-info">
-    <?php
-
-      get_template_part('template-parts/counsellors/booking-counsellor-information');
-
-    ?>
+    <?php get_template_part('template-parts/counsellors/booking-counsellor-information'); ?>
   </div>
 </div>
 <?php get_footer(); ?>
