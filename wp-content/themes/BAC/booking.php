8b6736c7e5951d2aa64f781b1673b685a790640a
d-horbachov-dev <dmytriy.horbachov@gmail.com>
2020-04-17 20:15:14 +0300

    home page form styles

--- wp-content/themes/BAC/booking.php
+++ wp-content/themes/BAC/booking.php
@@ -5,7 +5,7 @@
 <div class="booking-page mid-container d-flex">
   <div class="booking-form">
     <?php $userId = $_GET['user']; ?>
-    <?php echo do_shortcode('[ameliabooking employee='.$userId.']'); ?>
+    <?php echo do_shortcode('[ameliabooking]'); ?>
   </div>
   <div class="about-info">
     <?php get_template_part('template-parts/counsellors/booking-counsellor-information'); ?>
