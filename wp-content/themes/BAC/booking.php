c5c1c5fc3dd41159e37f73a66af874a7a6f9cf01
d-horbachov-dev <dmytriy.horbachov@gmail.com>
2020-08-14 17:35:58 +0300

    Added promo info on booking page

--- wp-content/themes/BAC/booking.php
+++ wp-content/themes/BAC/booking.php
@@ -4,12 +4,24 @@
 
 <div class="booking-page mid-container d-flex">
   <div class="booking-form">
+      <div class="text-block-800 text-block-800-consellors">
+          <p style="font-family: InterSemiBold, sans-serif; font-size:18px">
+              BAC PROMO
+          </p>
+          <p>
+              Book 5 sessions and save 5% using Coupon Code "<strong>SAVE 5</strong>".
+          </p>
+          <p>
+              Book 10 sessions and save 10% using Coupon Code "<strong>SAVE10</strong>"
+          </p>
+      </div>
     <?php
         $userId = $_GET['user'];
         $service = \BAC\Service::getAllByPractitionerId($userId);
         $shortcode = '[ameliabooking employee='.$userId.' service=' . $service[0]->id . ']';
     ?>
     <?php echo do_shortcode($shortcode); ?>
+
   </div>
   <div class="about-info">
     <?php get_template_part('template-parts/counsellors/single', 'booking'); ?>
