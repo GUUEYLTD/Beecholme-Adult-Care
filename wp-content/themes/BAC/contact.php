a42b320008cf894e71ddf51997cf66c1efab7086
Maxim Yashenko <dnc.maxim.yaschenko@gmail.com>
2020-12-03 16:13:37 +0200

    Contact page add styles

--- wp-content/themes/BAC/contact.php
+++ wp-content/themes/BAC/contact.php
@@ -1,17 +1,74 @@
 <?php /*Template Name: contact*/
  get_header();
-  ?>
+?>
+<style>
+    .contact form{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    .contact form p{
+        margin-bottom:25px
+    }
+    .contact form p, .contact form label{
+        width:100%
+    }
+    .contact .wpcf7-form-control-wrap{margin:auto}
+    .contact .wpcf7-form input[type="submit"]{
+        padding:10px 0;
+        max-width: 310px;
+    }
+    .contact div.wpcf7 .ajax-loader{
+        display:none!important
+    }
+    .contact .wpcf7-form input, .contact .wpcf7-form select, .contact .wpcf7-form textarea{
+        width:100%!important
+    }
 
+    .contact span.wpcf7-not-valid-tip {
+        color: #f00;
+        font-weight: normal;
+        display: block;
+        position: absolute;
+        font-size: 12px;
+    }
+    .contact div.wpcf7-response-output{
+        margin:30px auto;
+        text-align:center;
+    }
+
+    @media all and (min-width: 992px) {
+        .contact form p:nth-child(1),
+        .contact form p:nth-child(2),
+        .contact form p:nth-child(3),
+        .contact form p:nth-child(4),
+        .contact form p:nth-child(5){
+            width:48%
+        }
+        .contact-form-container{width:800px}
+    }
+    @media all and (max-width: 991px) {
+        .small-container{
+            padding:0 15px;
+        }
+    }
+    @media all and (max-width: 374px) {
+        .small-container{
+            padding:0;
+        }
+    }
+
+</style>
 <!--<div class="google-map">-->
 <!--	<div class="google-map-overlay"></div>-->
 <!--	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2488.388272210313!2d-0.15435908423344974!3d51.41429387961996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760668a774df57%3A0xc25249c9ed825206!2sBeecholme+Adult+Care!5e0!3m2!1sru!2sua!4v1518099027832" width="1920" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>-->
 <!--</div>-->
 
 <div class="form-wrapper contact">
-	<div class="mobile-switcher">
-		<button class="switch-button contact-us-button active">Contact form</button>
-		<button class="switch-button book-visit-button">Book a visit</button>
-	</div>
+<!--	<div class="mobile-switcher">-->
+<!--		<button class="switch-button contact-us-button active">Contact form</button>-->
+<!--		<button class="switch-button book-visit-button">Book a visit</button>-->
+<!--	</div>-->
 	<div class="small-container d-flex justify-content-center">
 		<div class="contact-form-container contact-us-form ">
 			<h4>Contact Form</h4>
